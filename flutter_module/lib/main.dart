0de81a2669d9a029cb70e944ed65dabb735e329c
Siy-Wu <578063456@qq.com>
2020-02-20 16:14:15 +0800

    item视图

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -34,29 +34,67 @@ class MyApp extends StatelessWidget {
 class ListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(children: <Widget>[
-      Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Image.asset(
-            "assets/image/timeline_like_normal.png",
-            width: 15.0,
-            height: 15.0,
-          )),
-      Column(
-        children: <Widget>[
-          Text(
-            "测试文字",
-            style: TextStyle(fontSize: 12.0, color: Color(0xff000000)),
-          ),
-          Row(children: <Widget>[
-            Text("测试",
-                style: TextStyle(
-                  fontSize: 12.0,
-                  color: Color(0xff000000),
-                ))
-          ])
-        ],
-      )
-    ]);
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+      child: Row(children: <Widget>[
+        Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Image.asset(
+              "assets/image/timeline_like_normal.png",
+              width: 15.0,
+              height: 15.0,
+            )),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
+              child: Text(
+                "测试文字",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 14.0, color: Color(0xff000000)),
+              ),
+            ),
+            Row(children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Color.fromARGB(255, 7, 157, 101),
+                              width: 1)),
+                      child: Text("问答",
+                          style: TextStyle(
+                            fontSize: 10.0,
+                            color: Color.fromARGB(255, 7, 157, 101),
+                          )))),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+                child: Text("作者：小样",
+                    style: TextStyle(
+                      fontSize: 10.0,
+                      color: Color(0xff000000),
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                child: Text("分类：问答/官方",
+                    style: TextStyle(
+                      fontSize: 10.0,
+                      color: Color(0xff000000),
+                    )),
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                  child: Text("时间：2020-2-20",
+                      style: TextStyle(
+                        fontSize: 10.0,
+                        color: Color(0xff000000),
+                      ))),
+            ])
+          ],
+        )
+      ]),
+    );
   }
 }
