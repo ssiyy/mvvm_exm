567c4cf72eee2c0a9c32eed621828e360acbd285
Siy <578063456@qq.com>
2020-03-23 21:59:19 +0800

    完善flutter webview代码

--- flutter_module/lib/ui/main_ui.dart
+++ flutter_module/lib/ui/main_ui.dart
@@ -59,7 +59,7 @@ class _MyAppState extends State<MyApp> {
   Future<ProblemPage> getPageData(int pageIndex) async {
     var problemPage =
         await HttpService.instance.get("/wenda/list/$pageIndex/json", fromJson :(map){
-          return ProblemPage.fromJson(map);
+          return ProblemPage.fromJson(map.data);
         });
     return problemPage;
   }
